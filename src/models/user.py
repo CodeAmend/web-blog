8098f071dd27e64fbda2be783f108047f339a0a8
Michael Bruce Allen <CodeAmend@Gmail.Com>
2016-01-14 10:56:36 -0500

    added uuid().hex to self._id in __init__

--- src/models/user.py
+++ src/models/user.py
@@ -3,10 +3,10 @@ from src.common.database import Database
 
 class User:
 
-    def __init__(self, email, password, _id):
+    def __init__(self, email, password, _id=None):
         self.email = email
         self.password = password
-        self._id = _id
+        self._id = uuid.uuid4().hex if _id is None else _id
 
     @classmethod
     def get_by_email(cls, email):
